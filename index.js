62486e1995a45943a54899739b2f42790d0c65ce
Munazza sultana <sultanamunazza3@gmail.com>
2025-05-20 02:49:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,6 +11,31 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+let userName = "";
+app.post("/api/users", (req, res) => {
+  userName = req.body.username;
+  res.json({
+    username : userName,
+    _id : Math.floor(Math.random() * 1000000)
+  })
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const userId = req.params._id;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  const date = req.body.date;
+  //const name = req.body.username;
+
+  res.json({
+    _id : userId,
+    username : userName,
+    date : date ? new Date(date).toDateString() : new Date().toDateString(),
+    duration : duration,
+    description : description
+    
+  })
+})
 
 
 
